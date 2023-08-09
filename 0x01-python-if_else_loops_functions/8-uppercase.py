bbbc153c7e3d46df527499283ec1c4a6fa6fa742
Abraham Airende <aoairende@gmail.com>
2023-08-08 18:49:37 -0700

    if else loops

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uppercase(str):
+    i = 0
+    ch = ""
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+            ch = chr(ord(i) - 32)
+        else:
+            ch = i
+        print("{0}".format(ch), end="")
+    print()
---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number < 0:
+        ld = (number * -1) % 10
+    else:
+        ld = number % 10
+    print("{0}".format(ld), end="")
+
+    return ld
