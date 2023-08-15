dece67977f33b2001bb6cebce8f103940d62ce01
Abraham Airende <aoairende@gmail.com>
2023-08-14 18:18:34 -0700

    Linked list palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,84 +1,37 @@
 #include "lists.h"
 #include <stdio.h>
 
-int listint_len(const listint_t *h);
-void copy_list_values(const listint_t *h, int *arr);
-int is_palindrome_helper(int *arr, int i, int j);
+/**
+ * palindrome_recursive - recursively check if it is a palindrome
+ *
+ * @h: pointer to left node
+ * @n: right node
+ *
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+int palindrome_recursive(listint_t **h, listint_t *n)
+{
+	int i;
+
+	if (n == NULL)
+		return (1);
+	i = palindrome_recursive(h, n->next);
+	if (i == 0)
+		return (0);
+	i = (n->n == (*h)->n);
+	*h = (*h)->next;
+
+	return (i);
+}
 
 /**
- * is_palindrome - inserts a new node in a sorted list
- * @head: pointer to head pointer to the list
- * Return: 1 if palindrom, 0 otherwise
+ * is_palindrome - checks if a singly linked list is a palindrome.
+ *
+ * @head: pointer to the list head
+ *
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
  */
 int is_palindrome(listint_t **head)
 {
-	int ans;
-	int list_len = listint_len(*head);
-	int *arr = malloc(sizeof(int) * list_len);
-
-	if (!*head)
-		return (1);
-
-	copy_list_values(*head, arr);
-	ans = is_palindrome_helper(arr, 0, list_len - 1);
-	free(arr);
-
-	return (ans);
-}
-
-/**
- * listint_len - returns the number of elements in a linked listint_t list
- * @h: pointer to the list
- * Return: number of elements in the list
- */
-int listint_len(const listint_t *h)
-{
-	int length = 0;
-	const listint_t *current_node = NULL;
-
-	if (!h)
-		return (length);
-	current_node = h;
-	while (current_node)
-	{
-		length++;
-		current_node = current_node->next;
-	}
-	return (length);
-}
-/**
- * copy_list_values - copy list into array
- * @h: pointer to the list
- * @arr: array to store int values from list
- */
-void copy_list_values(const listint_t *h, int *arr)
-{
-	int i = 0;
-	const listint_t *current_node = NULL;
-
-	if (!h)
-		return;
-	current_node = h;
-	while (current_node)
-	{
-		arr[i] = current_node->n;
-		i++;
-		current_node = current_node->next;
-	}
-}
-
-/**
- * is_palindrome_helper - recursively checks if a string is palindrome
- * @arr: array to check
- * @i: current position at the beginning of the string
- * @j: current position at end of the string
- * Return: returns 1 if palindrome, 0 otherwise
- */
-int is_palindrome_helper(int *arr, int i, int j)
-{
-	if (i >= j)
-		return (1);
-	if (arr[i] != arr[j])
-		return (0);
-	return (is_palindrome_helper(arr, i + 1, j - 1));
+	return (palindrome_recursive(head, *head));
 }
