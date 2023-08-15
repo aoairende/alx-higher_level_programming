b1a5a5f7954a6726b0bdfc766665d43e62ce5865
Abraham Airende <aoairende@gmail.com>
2023-08-14 19:12:06 -0700

    Delete at

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,8 +3,9 @@
 def delete_at(my_list=[], idx=0):
 
     # # Check if the index is within the valid range of the list.
-    if idx >= 0 and idx < len(my_list):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
 
         # Replace the slice at the specified index with an empty list to delete the element.
-        my_list[idx:(idx + 1)] = []
+        del my_list[idx]
     return my_list
