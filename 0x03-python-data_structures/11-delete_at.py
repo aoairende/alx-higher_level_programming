18aab2a8581536cda68ae8b2ab571069a98df2c7
Abraham Airende <aoairende@gmail.com>
2023-08-14 19:13:26 -0700

    Delete at

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,7 +2,7 @@
 
 def delete_at(my_list=[], idx=0):
 
-    # # Check if the index is within the valid range of the list.
+    # Check if the index is within the valid range of the list.
     if idx < 0 or idx >= len(my_list):
         return my_list
 
