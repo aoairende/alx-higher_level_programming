77c1db1b8792d3712c9fe60926569d32bf7d8fbd
Abraham Airende <aoairende@gmail.com>
2023-08-14 19:07:57 -0700

    Tuples addition

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,25 +1,19 @@
 #!/usr/bin/python3
 
+# adds 2 tuples.
 def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) < 2:
+        tuple_a += (0, 0)
+    if len(tuple_b) < 2:
+        tuple_b += (0, 0)
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
 
-    # Initialize variables to store the values from the tuples.
-    arg1_a = 0
-    arg1_b = 0
-    arg2_a = 0
-    arg2_b = 0
 
-    # Check the length of tuple_a to determine how to assign values to the variables.
-    if len(tuple_a) == 1:
-        arg1_a = tuple_a[0]
-    elif len(tuple_a) > 1:
-        arg1_a = tuple_a[0]
-        arg1_b = tuple_a[1]
+if __name__ == "__main__":
+    tuple_a = (1, 89)
+    tuple_b = (88, 11)
+    new_tuple = add_tuple(tuple_a, tuple_b)
+    print(new_tuple)
 
-    # Check the length of tuple_b to determine how to assign values to the variables.
-    if len(tuple_b) == 1:
-        arg2_a = tuple_b[0]
-    elif len(tuple_b) > 1:
-        arg2_a = tuple_b[0]
-        arg2_b = tuple_b[1]
-
-    return (arg1_a + arg2_a, arg1_b + arg2_b)
+    print(add_tuple(tuple_a, (1, )))
+    print(add_tuple(tuple_a, ()))
