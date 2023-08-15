02ce9dfd4c02654f869e645fa964693b05f98d93
Abraham Airende <aoairende@gmail.com>
2023-08-14 18:31:23 -0700

    Linked list palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -2,20 +2,26 @@
 #include <stdio.h>
 
 /**
- * palindrome_recursive - recursively check if it is a palindrome
+ * check_palindrome_recursive - recursively check if it is a palindrome
  *
- * @h: pointer to left node
- * @n: right node
+ * @h: pointer to the head of the linked list.
+ * @n: pointer to the current node been checked.
  *
- * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome.
  */
-int palindrome_recursive(listint_t **h, listint_t *n)
+
+int check_palindrome_recursive(listint_t **h, listint_t *n)
 {
 	int i;
 
+	// If the current node is NULL, we have reached the end of the list.
 	if (n == NULL)
 		return (1);
-	i = palindrome_recursive(h, n->next);
+
+	// Recursively call the function for the next node in the list.
+	i = check_palindrome_recursive(h, n->next);
+
+	// If i is 0, the previous recursive call detected a mismatch, so return 0
 	if (i == 0)
 		return (0);
 	i = (n->n == (*h)->n);
@@ -27,11 +33,12 @@ int palindrome_recursive(listint_t **h, listint_t *n)
 /**
  * is_palindrome - checks if a singly linked list is a palindrome.
  *
- * @head: pointer to the list head
+ * @head: pointer to the linked list.
  *
  * Return: 0 if it is not a palindrome, 1 if it is a palindrome
  */
+
 int is_palindrome(listint_t **head)
 {
-	return (palindrome_recursive(head, *head));
+	return (check_palindrome_recursive(head, *head));
 }
