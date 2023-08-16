990f061cced7716b9cc95ae82c7ee198313aa3b8
Abraham Airende <aoairende@gmail.com>
2023-08-16 15:04:57 -0700

    Update dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+
+    # Update or add the specified key with the given value.
+    a_dictionary[key] = value
+    # Return the updated dictionary
+    return a_dictionary
