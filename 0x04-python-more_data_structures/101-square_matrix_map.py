21b45cf41acf86eed05360dc1702f130f3430f4f
Abraham Airende <aoairende@gmail.com>
2023-08-16 15:47:11 -0700

    Squared by using map

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-
 def square_matrix_map(matrix=[]):
-    # Use nested 'map' functions to create a new matrix with elements squared.
     return list(map(lambda row: list(map(lambda col: col**2, row)), matrix))
