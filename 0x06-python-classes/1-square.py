cac378ff6a468d41f06d809fab1d242f12c61d1f
aoairende <aoairende@gmail.com>
2023-08-29 23:32:54 +0000

    Suare with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Defines a square class """
+
+
+class Square:
+    """Defines a sqaure class"""
+
+    def __init__(self, size):
+        """Initialize a new square"""
+
+        self.__size = size
