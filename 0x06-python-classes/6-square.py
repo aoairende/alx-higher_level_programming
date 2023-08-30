0d36ad8099f071b3cfda6c8f5a1fcf37d9755359
aoairende <aoairende@gmail.com>
2023-08-30 00:17:44 +0000

    Coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -5,14 +5,11 @@
 class Square:
     """ Defines a square class """
 
-    def __init__(self, size=0):
-        """Initializes a new square """
+    def __init__(self, size=0, position=(0, 0)):
+        """ Initializes a new square """
 
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+        self.size = size
+        self.position = position
 
     def area(self):
         """ Returns the area of the square """
@@ -21,20 +18,49 @@ class Square:
 
     @property
     def size(self):
-        """ Returns size of square """
+        """ Returns the size of the square """
 
         return self.__size
 
     @size.setter
     def size(self, value):
-        """ Sets the value of size """
+        """Set the value of size"""
 
-        self.__init__(value)
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """ Returns the position of the square """
+
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """ Sets the value of position """
+
+        if not (
+            isinstance(value, tuple)
+            and len(value) == 2
+            and isinstance(value[0], int)
+            and isinstance(value[1], int)
+            and value[0] >= 0
+            and value[1] >= 0
+        ):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
 
     def my_print(self):
         """ Prints square using the # """
 
         if self.__size == 0:
             print()
-        for i in range(self.__size):
-            print(self.__size * "#")
+        else:
+            for i in range(self.__position[1]):
+                print()
+            for i in range(self.__size):
+                print(self.__position[0] * " ", end="")
+                print(self.__size * "#")
