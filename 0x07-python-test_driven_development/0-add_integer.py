b14f941d3223a05ca6912c03503f3efedaf4bc32
aoairende <aoairende@gmail.com>
2023-09-01 01:02:50 +0100

    Integers addition

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+""" Module defines a function to add two integers """
+
+
+def add_integer(a, b=98):
+    """ Adds two integers """
+
+    # Check if 'a' is not an integer or float, & raise a TypeError if its not.
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+
+    # Check if 'b' is not an integer or float, and raise a TypeError if its not
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+
+    # Convert 'a' and 'b' to integers and return their sum.
+    return int(a) + int(b)
